6be4fa4542b02f8c2ea9e416f8c2d9df23e40345
pt  qu <496887756@qq.com>
2022-11-12 14:00:44 +0800

    1

---
+++ 001/go.mod
@@ -0,0 +1,5 @@
+module 001
+
+go 1.18
+
+require github.com/zeromicro/go-zero v1.4.2 // indirect
--- 001/main.go
+++ 001/main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"io/fs"
 	"log"
 	"net"
 	"net/http"
 	"os"
+
+	"github.com/zeromicro/go-zero/core/stringx"
 )
 
 func main() {
+
+	filter := stringx.NewTrie([]string{
+		"AV演员",
+		"苍井空",
+		"AV",
+		"日本AV女优",
+		"AV演员色情",
+	}, stringx.WithMask('?'))
+	safe, keywords, found := filter.Filter("日本AV演员兼电视、电影演员。苍井空AV女优是xx出道, 日本AV女优们最精彩的表演是AV演员色情表演")
+	fmt.Println(safe)
+	fmt.Println(keywords)
+	fmt.Println(found)
+
+
 	http.Handle("/", http.FileServer(getFileSystem(false)))
 	ip, err := getLocalIP()
 	if err != nil {
