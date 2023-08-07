be127f800649ece0a890e1e77cbd0f59ea49d197
pt  qu <ptqu@betawm.com>
2023-08-07 18:25:26 +0800

    lb

--- es/main.go
+++ es/main.go
@@ -1,164 +1,198 @@
 package main
 
 import (
-	"bytes"
+	_ "bytes"
 	"context"
-	"encoding/json"
-	"flag"
+	_ "encoding/json"
+	_ "flag"
 	"fmt"
+	"log"
 	_ "log"
 	_ "time"
 
-	"github.com/elastic/go-elasticsearch/v7"
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/typedapi/types"
 )
 
-func Loges(msg string, dateIndex string) {
+// func Loges(msg string, dateIndex string) {
 
-	addresses := []string{"http://es-dev.test.betawm.com:9201/"}
+// 	addresses := []string{"http://es-dev.test.betawm.com:9201/"}
 
-	config := elasticsearch.Config{
+// 	config := elasticsearch.Config{
 
-		Addresses: addresses,
-	}
+// 		Addresses: addresses,
+// 	}
 
-	// new client
+// 	// new client
 
-	es, err := elasticsearch.NewClient(config)
-	if err == nil {
-		fmt.Println(err, "Error creating the client")
-	}
-	res, err := es.Info()
+// 	es, err := elasticsearch.NewClient(config)
+// 	if err == nil {
+// 		fmt.Println(err, "Error creating the client")
+// 	}
+// 	res, err := es.Info()
 
-	if err == nil {
+// 	if err == nil {
 
-		fmt.Println(err, "Error getting response")
+// 		fmt.Println(err, "Error getting response")
 
-	}
-	// fmt.Println(res.String())
+// 	}
+// 	// fmt.Println(res.String())
 
-	var buf bytes.Buffer
+// 	var buf bytes.Buffer
 
-	query := map[string]interface{}{
+// 	query := map[string]interface{}{
 
-		"query": map[string]interface{}{
+// 		"query": map[string]interface{}{
 
-			"match": map[string]interface{}{
+// 			"match": map[string]interface{}{
 
-				"companyNameIK": "中银",
-			},
-		},
+// 				"companyNameIK": "中银",
+// 			},
+// 		},
 
-		"highlight": map[string]interface{}{
+// 		"highlight": map[string]interface{}{
 
-			"pre_tags": []string{"<font color='red'>"},
+// 			"pre_tags": []string{"<font color='red'>"},
 
-			"post_tags": []string{"</font>"},
+// 			"post_tags": []string{"</font>"},
 
-			"fields": map[string]interface{}{
+// 			"fields": map[string]interface{}{
 
-				"companyNameIK": map[string]interface{}{},
-			},
-		},
-	}
+// 				"companyNameIK": map[string]interface{}{},
+// 			},
+// 		},
+// 	}
 
-	if err := json.NewEncoder(&buf).Encode(query); err != nil {
+// 	if err := json.NewEncoder(&buf).Encode(query); err != nil {
 
-		fmt.Println(err, "Error encoding query")
+// 		fmt.Println(err, "Error encoding query")
 
-	}
+// 	}
 
-	// Perform the search request.
+// 	// Perform the search request.
 
-	res, err = es.Search(
+// 	res, err = es.Search(
 
-		es.Search.WithContext(context.Background()),
+// 		es.Search.WithContext(context.Background()),
 
-		es.Search.WithIndex("fundfiltering"),
+// 		es.Search.WithIndex("fundfiltering"),
 
-		es.Search.WithBody(&buf),
+// 		es.Search.WithBody(&buf),
 
-		es.Search.WithTrackTotalHits(true),
+// 		es.Search.WithTrackTotalHits(true),
 
-		es.Search.WithPretty(),
-	)
+// 		es.Search.WithPretty(),
+// 	)
 
-	if err != nil {
+// 	if err != nil {
 
-		fmt.Println(err, "Error getting response")
+// 		fmt.Println(err, "Error getting response")
 
-	}
+// 	}
 
-	defer res.Body.Close()
+// 	defer res.Body.Close()
 
-	// return res.String()
+// 	// return res.String()
 
-	fmt.Println(res.String())
+// 	fmt.Println(res.String())
 
-}
+// }
 
-var (
-	msg string
+// var (
+// 	msg string
 
-	dateIndex string
-)
+// 	dateIndex string
+// )
 
-func init() {
+// func init() {
 
-	flag.StringVar(&msg, "msg", "检验是否满足xx条件", "输入要匹配的内容")
+// 	flag.StringVar(&msg, "msg", "检验是否满足xx条件", "输入要匹配的内容")
 
-	flag.StringVar(&dateIndex, "dateIndex", "product", "输入要搜索的索引")
+// 	flag.StringVar(&dateIndex, "dateIndex", "product", "输入要搜索的索引")
 
+// }
+
+// func main() {
+
+// 	flag.Parse()
+
+// 	Loges(msg, dateIndex)
+
+// }
+
+type Student struct {
+	BetaCode string
+	Name     string
+	Score    float64
 }
 
 func main() {
+	// 创建 Elasticsearch 配置
+	cfg := elasticsearch.Config{
+		Addresses:               []string{"http://localhost:9200"}, // Elasticsearch 节点的地址
+		EnableCompatibilityMode: false,
+	}
 
-	flag.Parse()
+	// 创建 Elasticsearch 客户端
+	typedClient, err := elasticsearch.NewTypedClient(cfg)
+	if err != nil {
+		log.Fatalf("Error creating the client: %s", err)
+	}
+	inf, err := typedClient.Info().Do(context.Background())
+	if err == nil {
+		fmt.Println("info", inf.Version)
+	}
+	// settings := map[string]interface{}{
+	// 	"index.number_of_replicas": 2,
+	// 	"index.refresh_interval":   "30s",
+	// 	// 添加其他要更改的设置
+	// }
+	// 将 map 转换成 JSON 字符串
+	// jsonData, err := json.Marshal(settings)
+	// if err != nil {
+	// 	fmt.Println("Error marshaling data to JSON:", err)
+	// 	return
+	// }
 
-	Loges(msg, dateIndex)
+	// 执行 Elasticsearch 查询
+	//  _ = bytes.NewReader(jsonData)
+
+	var a bool = true
+
+	mapping := &types.TypeMapping{
+
+		Properties: map[string]types.Property{
+			"BetaCode": types.TextProperty{
+				Index: &a,
+			},
+			"Name":  types.TextProperty{},
+			"Score": types.FloatNumberProperty{},
+		},
+	}
+
+	indexName := "45666"
+
+	exist, err := typedClient.Indices.Exists(indexName).Do(context.Background())
+
+	if err != nil && !exist {
+
+		res1, _ := typedClient.Indices.Create(indexName).Mappings(mapping).Do(context.Background())
+
+		if err != nil {
+			log.Fatalf("Error executing the request: %s", err)
+		}
+
+		// 处理查询结果
+		fmt.Println(res1)
+	}
+
+	bulk, _ := typedClient.Core.Create(indexName, "122").Document(Student{
+		BetaCode: "122",
+		Name:     "qpt",
+		Score:    80,
+	}).Do(context.Background())
+
+	// 处理查询结果
+	fmt.Println(bulk)
 
 }
-
-// func main() {
-// 	// 创建 Elasticsearch 配置
-// 	cfg := elasticsearch.Config{
-// 		Addresses: []string{"http://es-dev.test.betawm.com:9201/"}, // Elasticsearch 节点的地址
-// 	}
-
-// 	// 创建 Elasticsearch 客户端
-// 	typedClient, err := elasticsearch.NewTypedClient(cfg)
-// 	if err != nil {
-// 		log.Fatalf("Error creating the client: %s", err)
-// 	}
-// 	inf, err := typedClient.Info().Do(context.Background())
-// 	if err == nil {
-// 		fmt.Println("info", inf.Version)
-// 	}
-// 	// settings := map[string]interface{}{
-// 	// 	"index.number_of_replicas": 2,
-// 	// 	"index.refresh_interval":   "30s",
-// 	// 	// 添加其他要更改的设置
-// 	// }
-// 	// 将 map 转换成 JSON 字符串
-// 	// jsonData, err := json.Marshal(settings)
-// 	// if err != nil {
-// 	// 	fmt.Println("Error marshaling data to JSON:", err)
-// 	// 	return
-// 	// }
-
-// 	// 执行 Elasticsearch 查询
-// 	//  _ = bytes.NewReader(jsonData)
-
-// 	insL := &types.IndexSettings{
-// 		NumberOfReplicas: "2",
-// 		NumberOfShards:   "2",
-// 	}
-
-// 	res, err := typedClient.Indices.PutSettings().Index("45666").Request(insL).Do(context.Background())
-
-// 	if err != nil {
-// 		log.Fatalf("Error executing the request: %s", err)
-// 	}
-
-// 	// 处理查询结果
-// 	fmt.Println(res)
-// }
